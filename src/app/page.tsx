3390f9c8c9037343d56e669e1f516018cab43379
Peerasak Saetan <31280221+FordenHillson@users.noreply.github.com>
2023-10-05 01:37:49 +0700

    mobile design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,79 @@
-import Image from 'next/image'
-
+import Image from "next/image";
+import data from "../../data.json";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="flex min-h-screen flex-col justify-start bg-white font-sans">
+      <div className="bg-gradient-to-t from-light-royal-blue to-light-slate-blue justify-center rounded-b-[3rem] mb-5">
+        <p className="text-lg font-semibold text-center text-light-lavender py-8">
+          Your Result
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className="relative flex flex-col mx-auto rounded-full w-40 h-40 items-center text-cente justify-center bg-gradient-to-b from-violet-blue to-persian-blue mb-4">
+          <p className="text-6xl font-bold text-center py-4">76</p>
+          <p className="font-semibold text-center text-light-lavender text-base">
+            of 100
+          </p>
         </div>
+        <p className="font-medium text-center text-2xl pb-2">Great</p>
+        <p className="font-medium text-center px-20 pb-10 text-light-lavender">
+          You scored higher than 65% of the people who have taken these tests.
+        </p>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="text-black px-8">
+        <p className=" font-extrabold text-xl pb-4">Summary</p>
+        <div className=" font-bold">
+          <div className=" bg-red-50 mb-3 p-4 rounded-lg flex justify-between">
+            <div className="flex">
+              <Image src={data[0].icon} alt="icon" width={28} height={28} className="pr-2" />
+              <p className=" text-light-red">{(data[0].category)}</p>
+            </div>
+            <div className="flex font-black">
+              <p className="pr-2 text-dark-gray-blue">{(data[0].score)}</p>
+              <p className=" text-slate-400">/ 100</p>
+            </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className=" bg-yellow-50 mb-3 p-4 rounded-lg flex justify-between">
+            <div className="flex">
+              <Image src={data[1].icon} alt="icon" width={28} height={28} className="pr-2" />
+              <p className=" text-orangey-yellow">{(data[1].category)}</p>
+            </div>
+            <div className="flex font-black">
+              <p className="pr-2 text-dark-gray-blue">{(data[1].score)}</p>
+              <p className=" text-slate-400">/ 100</p>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <div className=" bg-green-50 mb-3 p-4 rounded-lg flex justify-between">
+            <div className="flex">
+              <Image src={data[2].icon} alt="icon" width={28} height={28} className="pr-2" />
+              <p className=" text-green-teal">{(data[1].category)}</p>
+            </div>
+            <div className="flex font-black">
+              <p className="pr-2 text-dark-gray-blue">{(data[2].score)}</p>
+              <p className=" text-slate-400">/ 100</p>
+            </div>
+
+          </div>
+
+          <div className=" bg-blue-50 mb-3 p-4 rounded-lg flex justify-between">
+            <div className="flex">
+              <Image src={data[3].icon} alt="icon" width={28} height={28} className="pr-2" />
+              <p className=" text-cobalt-blue">{(data[3].category)}</p>
+            </div>
+            <div className="flex font-black">
+              <p className="pr-2 text-dark-gray-blue">{(data[3].score)}</p>
+              <p className=" text-slate-400">/ 100</p>
+            </div>
+
+          </div>
+
+        </div>
+
+        <button className=" bg-dark-gray-blue w-full p-3 font-bold text-xl text-white rounded-full mt-2">Continute</button>
       </div>
     </main>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,14 +6,30 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
+  theme: {     
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-    },
+      colors: {
+        'light-slate-blue': 'hsl(252, 100%, 67%)',
+        'light-royal-blue': 'hsl(241, 81%, 54%)',
+        'light-lavender': 'hsl(241, 100%, 89%)',
+        'dark-gray-blue': 'hsl(224, 30%, 27%)',
+        'persian-blue': 'hsla(241, 72%, 46%, 0)',
+        'violet-blue': 'hsla(256, 72%, 46%, 1)',
+        'light-red': 'hsl(0, 100%, 67%)',
+        'orangey-yellow' : 'hsl(39, 100%, 56%)',
+        'green-teal' : 'hsl(166, 100%, 37%)',
+        'cobalt-blue' : 'hsl(234, 85%, 45%)',
+        'pale-blue' : 'hsl(221, 100%, 96%)'
+      },
+      fontFamily: {
+        sans:['var(--font-inter)'],
+      }
+    },    
   },
   plugins: [],
 }
